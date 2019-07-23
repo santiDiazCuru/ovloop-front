f457a7210604c52e270a9c14ef44faef605902eb
Santiago <santidiazcurutchet@gmail.comsantidiazcurutchet@gmail.comgit config --global user.email santidiazcurutchet@gmail.com>
2019-07-23 16:57:08 -0300

    algo de front

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ mongoose.set('useCreateIndex', true);
 
 
 // CONNECT TO DB (con docker!!!!)
-mongoose.connect("mongodb://mongo:27017/dashboard")
-    .then(() => console.log('mongoDB connected to ovloop dashboard'))
-    .catch(err => console.log('db error: ', err));
+// mongoose.connect("mongodb://mongo:27017/dashboard")
+//     .then(() => console.log('mongoDB connected to ovloop dashboard'))
+//     .catch(err => console.log('db error: ', err));
 
 
 //PASSPORT & SESSION
@@ -36,13 +36,13 @@ app.use(passport.session());
 
 app.use(cookieParser());
 
-// CONNECT TO DB (para localhost)
-// mongoose.connect("mongodb://localhost/dashboard")
-//     .then(() => console.log('mongoDB connected to ovloops dashboard'))
-//     .catch(err => console.log('db error: ', err));
+//CONNECT TO DB (para localhost)
+mongoose.connect("mongodb://localhost/dashboard")
+    .then(() => console.log('mongoDB connected to ovloops dashboard'))
+    .catch(err => console.log('db error: ', err));
 
 // SETTTINGS
-app.set("port", process.env.PORT || 7777);
+app.set("port", process.env.PORT || 7770);
 
 // MIDDLEWARES
 app.use(express.static(path.join(__dirname, './server/public')));
--- server/routes/stats.js
+++ server/routes/stats.js
@@ -2,18 +2,31 @@ const express = require('express');
 const router = express.Router();
 const Axios = require('axios')
 
-const host = `http://${process.env.API_HOST}:${process.env.API_PORT}`;
+//const host = `http://${process.env.API_HOST}:${process.env.API_PORT}`;
+
+// router.post('/', (req, res) => {
+//     return Axios.post(`${host}/stats`, req.body)
+//         .then(msgs => res.json(msgs.data))
+// })
+// router.get('/getchannels', (req, res) => {
+//     return Axios.get(`${host}/stats/getchannels`)
+//         .then(channels => res.json(channels.data))
+// })
+// router.get('/getorigins', (req, res) => {
+//     return Axios.get(`${host}/stats/getorigins`)
+//         .then(origins => res.json(origins.data ))
+// })
 
 router.post('/', (req, res) => {
-    return Axios.post(`${host}/stats`, req.body)
+    return Axios.post(`http:localhost:8080/stats`, req.body)
         .then(msgs => res.json(msgs.data))
 })
 router.get('/getchannels', (req, res) => {
-    return Axios.get(`${host}/stats/getchannels`)
+    return Axios.get(`http:localhost:8080/stats/getchannels`)
         .then(channels => res.json(channels.data))
 })
 router.get('/getorigins', (req, res) => {
-    return Axios.get(`${host}/stats/getorigins`)
+    return Axios.get(`http:localhost:8080/stats/getorigins`)
         .then(origins => res.json(origins.data ))
 })
 
